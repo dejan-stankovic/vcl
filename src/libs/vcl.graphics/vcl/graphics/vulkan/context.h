267a5d1528f7be09b6a3c11f1fd4e32691082a1d
Basil Fierz <basil.fierz@hotmail.com>
2018-08-27 20:19:42 +0200

    Add graphics support for Vulkan

---
+++ src/libs/vcl.graphics/vcl/graphics/vulkan/context.h
@@ -0,0 +1,97 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2016 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+// VCL configuration
+#include <vcl/config/global.h>
+
+// C++ standard library
+#include <array>
+#include <string>
+#include <vector>
+
+// Vulkan
+#include <vulkan/vulkan.h>
+
+// GSL
+#include <gsl/gsl>
+
+namespace Vcl { namespace Graphics { namespace Vulkan
+{
+	class Device;
+
+	struct ContextQueueInfo
+	{
+		//! Index of the vulkan queue family
+		uint32_t FamilyIndex;
+	};
+
+	enum class CommandBufferType
+	{
+		Default = 0,
+		Static = 1,
+		Transient = 2
+	};
+	
+	class Context final
+	{
+	public:
+		//! Constructor
+		Context(Device* dev, gsl::span<const char*> layers, gsl::span<const char*> extensions);
+
+		//! Destructor
+		~Context();
+
+		//! Convert to Vulkan ID
+		inline operator VkDevice() const
+		{
+			return _device;
+		}
+
+		Device* device() const { return _physicalDevice; }
+		VkPipelineCache cache() const { return _pipelineCache; }
+
+		VkCommandPool commandPool(uint32_t queueIdx, CommandBufferType type);
+
+	public:
+		VkQueue queue(VkQueueFlags flags, uint32_t idx);
+		
+	private:
+		//! Vulkan physical device
+		Device* _physicalDevice{ nullptr };
+
+		//! Vulkan device
+		VkDevice _device{ nullptr };
+
+		//! Queue families
+		std::vector<VkQueueFamilyProperties> _queueFamilies;
+
+		//! Associated pipeline cache
+		VkPipelineCache _pipelineCache;
+
+		//! Pre-allocated command pools
+		std::vector<std::array<VkCommandPool, 3>> _cmdPools;
+	};
+}}}
---
+++ src/tests/vcl.graphics.vulkan/test.h
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2018 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// VCL configuration
+#include <vcl/config/global.h>
+
+// C++ Standard Library
+
+// Include the relevant parts from the library
+#include <vcl/graphics/vulkan/context.h>
+#include <vcl/graphics/vulkan/device.h>
+#include <vcl/graphics/vulkan/platform.h>
+
+// Google test
+#include <gtest/gtest.h>
+
+class VulkanTest : public testing::Test
+{
+public:
+	void SetUp() override
+	{
+		using namespace Vcl::Graphics::Vulkan;
+
+		// Initialize the Vulkan platform
+		_platform = std::make_unique<Platform>();
+		auto& dev = _platform->device(0);
+		_context = dev.createContext();
+	}
+
+	void TearDown() override
+	{
+		_context.reset();
+		_platform.reset();
+	}
+
+protected:
+	std::unique_ptr<Vcl::Graphics::Vulkan::Platform> _platform;
+	std::unique_ptr<Vcl::Graphics::Vulkan::Context> _context;
+};
